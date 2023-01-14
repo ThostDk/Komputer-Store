046108c038efbc16db4659430e6981f3cbde2301
ThostDk <thomas.osterhammel@gmail.com>
2023-01-15 00:18:48 +0100

    added button functionality and code for updating and toggling HTML text content

--- js/app.js
+++ js/app.js
@@ -1,29 +1,16 @@
 import bank from "./bank.js"
-const getALoanElement = document.getElementById("balanceBtn")
-const bankBalanceElement = document.getElementById("balanceAmount")
+//Connecting HTML Buttons to the javascript
+const getALoanBtnElement = document.getElementById("getALoanBtn");
+const bankBtnElement = document.getElementById("bankBtn");
+const repayLoanBtnElement = document.getElementById("repayLoanBtn");
 
-getALoanElement.addEventListener("click", () => {
-    addToBalance()
-})
 
-function loanPrompt() {
-    let person = prompt("Please enter your name", "Harry Potter");
-    if (person != null) {
-        document.getElementById("demo").innerHTML =
-            "Hello " + person + "! How are you today?";
-    }
-}
 
-function addToBalance() {
-    let loanTmp = "";
-    while (true){
-        loanTmp = Number(prompt("how much do you want to loan?"));
-        if (isNaN(loanTmp) && loanTmp < 0 ){
-            loanTmp = Number(prompt("how much do you want to loan? (Write a Number bigger than zero)"));
-        }
-        break;
-    }
-    loanBalance += loanTmp;
-    bank.bankBalance += bank.loanBalance;
-    bankBalanceElement.textContent = bank.bankBalance + " kr";
-}
+getALoanBtnElement.addEventListener("click", () => {
+    bank.takeLoan();
+});
+repayLoanBtnElement.addEventListener("click", () => {
+    bank.repayLoan();
+});
+
+
--- js/bank.js
+++ js/bank.js
@@ -1,8 +1,108 @@
-let bankBalance = 0;
-let loanBalance = 0;
+let bankBalance = 500;
+let loanDebt = 1200;
+let pay = 1450;
+
+//Connecting HTML Text to the javascript
+const balanceAmountTextElement = document.getElementById("balanceAmount")
+const payAmountTextElement = document.getElementById("payAmount")
+const loanDebtAmountTextElement = document.getElementById("loanDebtAmount")
+const loanDebtDescriptionTextElement = document.getElementById("loanDebtDescription")
+
+//Connecting HTML Buttons to the javascript
+const repayLoanBtnElement = document.getElementById("repayLoanBtn")
+
+
+updateBankInfo()
+if(loanDebt <= 0){toggleLoanHtmlDisplay(false)}
+
+
 const bank = {
+    
     bankBalance,
-    loanBalance
+    loanDebt,
+    takeLoan,
+    repayLoan
+
 }
+//#region public functions
+function repayLoan() {
+    if(loanDebt > 0)
+    {
+        pay -=loanDebt
+        if(pay > 0)
+        {
+            bankBalance += pay;
+            loanDebt = 0;
+        }
+        else
+        {
+            loanDebt = Math.abs(pay)
+            pay = 0;
+        }
+        updateBankInfo();
+    }
+}
+
+
+function takeLoan() {
+    if(loanDebt <= 0)
+    {
+        let loanTmp = "";
+        while (true){
+            loanTmp = Number(prompt(`how much do you want to loan? max ${bankBalance*2}`));
+            if (isNaN(loanTmp) || loanTmp < 0 || loanTmp > bankBalance*2){
+                loanTmp = Number(prompt(`Write a Number that is bigger than zero & max ${bankBalance*2}`));
+            }
+            break;
+        }
+        loanDebt += loanTmp;
+        bankBalance += loanDebt;
+        toggleLoanHtmlDisplay(true)
+        loanDebtAmountTextElement.textContent = loanTmp;
+        balanceAmountTextElement.textContent = bankBalance + " kr";
+    }
+    else{alert(`Sorry, you still need to repay your previous loan of ${loanDebt} kr. before taking another loan`);}
+}
+
+
+function bankMoney() 
+{
+
+}
+
+//#endregion public functions
+
+//#region Private functions
+
+function updateBankInfo()
+{
+    loanDebtAmountTextElement.textContent   = `${loanDebt} kr`;
+    balanceAmountTextElement.textContent    = `${bankBalance} kr` ;
+    payAmountTextElement.textContent        = `${pay} kr`;
+    if(loanDebt <= 0){toggleLoanHtmlDisplay(false)}
+    else{toggleLoanHtmlDisplay(true)}
+}
+
+// function for toggling between showing/hiding Loan HTML text & buttons (Boolean Parameter)
+function toggleLoanHtmlDisplay(displayHTML)
+{
+    if(displayHTML)
+    {
+        loanDebtDescriptionTextElement.style.display = "";
+        loanDebtAmountTextElement.style.display = "";
+        repayLoanBtnElement.hidden = false;
+    }
+    else
+    {
+        loanDebtDescriptionTextElement.style.display = "none";
+        loanDebtAmountTextElement.style.display = "none";
+        repayLoanBtnElement.hidden = true;
+    }
+}
+
+//#endregion Private functions
+
+
+
 
 export default bank;
